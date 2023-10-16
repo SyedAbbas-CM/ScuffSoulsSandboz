d12e9270fc070dfdffef567a0e9a5ea1ec7be9e9
maksusik <142452632+maksusik@users.noreply.github.com>
2023-10-16 20:28:28 +0200

    platform
    platforma added

--- Assets/Scripts/Environment Scripts/Platform.cs
+++ Assets/Scripts/Environment Scripts/Platform.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Platform : MonoBehaviour //mks
@@ -7,8 +8,10 @@ public class Platform : MonoBehaviour //mks
 GameObject plyr_loc;
 int height;
 private float time;
+public float maxtimeup;
 public float maxtime;
-
+public string goup_fnct;
+public string godwn_fnct;
 void Awake() 
 {
 plyr_loc= GameObject.Find("Player");
@@ -17,13 +20,14 @@ height=1;
 
 void Update () 
 {
-
     if (height==1 || height==4){
     if(Vector3.Distance(transform.position,plyr_loc.transform.position)<0.85f) {
     elevatii();}}
-
-    else if (height==2){moveup();}
-    else {movedown();}
+}
+void FixedUpdate()
+{
+    if (height==2){moveup();}
+    else if (height==3) {movedown();}
 }
 
 void elevatii()
@@ -42,7 +46,7 @@ void moveup()
 {
 Debug.Log("moving up");
 time+=Time.deltaTime;
-transform.position+=new Vector3(0f,Time.deltaTime,0f);
+transform.position+=new Vector3(0f,Time.deltaTime*strng_t_nbmr(goup_fnct,time),0f);
 if (time>=maxtime) {height=4;}
 }
 
@@ -50,7 +54,16 @@ void movedown()
 {
 Debug.Log("moving down");
 time+=Time.deltaTime;
-transform.position+=new Vector3(0f,-Time.deltaTime,0f);
+transform.position+=new Vector3(0f,-Time.deltaTime*strng_t_nbmr(goup_fnct,time),0f);
 if (time>=maxtime) {height=1;}
 }
+
+private float strng_t_nbmr(string govert,float timex)
+{
+   govert = govert.Replace("x", timex.ToString());
+   govert=govert.Replace(",",".");
+   ExpressionEvaluator.Evaluate(govert, out float result);
+   return(result);
+}
+
 }
